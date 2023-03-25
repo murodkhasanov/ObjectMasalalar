a40c85147e06b0f9b3d6491f6fc0b2dd1e112f19
murodkhasanov <tt1953385@gmail.com>
2023-03-25 16:08:56 +0500

    Obyekt masalalari

---
+++ script.js
@@ -0,0 +1,50 @@
+//1
+//book nomli obyekt yarating
+let book = {
+    name:"O'tkan kunlar",
+    author:"Abdulla Avloniy",
+    "publish date": 1975 ,
+    pages:250
+}
+console.log(book);
+
+
+
+//1
+//computer nomli obyekt yarating
+let computer = {
+    name:"HP Laptop",
+    os:"Windows 11 pro",
+    display:"15.6",
+    processor:"Intel Core i5 1056U 12th gen",
+    ram:"8 GB DDR4",
+    "video memory":"RTX 1078 4GB ma'lumotlarni saqlash qurilmasi 256 GB SSD",
+}
+console.log(computer);
+
+
+
+//3
+//person nomli obyekt yarating
+let person = {
+    name:"Murod",
+    lastname:"Hasanov",
+    profession:"dasturchi",
+    age:16,
+    sex:"erkak",
+    mobile:973950899,
+    adress:"Ishtixon tuman",
+    mother: {
+        name:"Dilbar",
+        lastname:"Shavqiev",
+        profession:"tadbirkor",
+        birth:"03.04.1970",
+    },
+    father: {
+        name:"Shuhrat",
+        lastname:"Shavqiev",
+        profession:"tadbirkor",
+        birth:"23.07.1966"
+    }
+}
+console.log(person);
